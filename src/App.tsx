55b603ab10c8e8ca17085e1c4507eb3c67df3e65
Tina Homayouni <t.homayouni@vardastapp.com>
2023-06-14 04:17:29 +0330

    add direction property to card

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,16 @@ function App() {
   return (
     <>
       <h1>This is my app</h1>
-      <div></div>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <Card
+          direction="column"
+          text="salam this is text"
+          column={4}
+          boxShadow="yes"
+        >
+          <h3>Hi H3</h3>
+        </Card>
+      </div>
     </>
   );
 }
