5b0febc7ea724eea6c3a8c2b7b474d497a81b2d6
Tina Homayouni <t.homayouni@vardastapp.com>
2023-06-15 17:31:32 +0330

    image kit with style round

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import Image from "./uiKit/image/image";
 import sag from "./uiKit/card/default-image.jpeg";
 function App() {
-  return (
-    <>
-      <Image src={sag} />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
