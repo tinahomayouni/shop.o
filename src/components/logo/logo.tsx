6f37083416050cf4eb4865816943ad45df8d107f
Tina Homayouni <t.homayouni@vardastapp.com>
2023-06-17 04:48:06 +0330

    add logo . modify Image kit

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,10 @@
+import Image from "../../uiKit/image/image";
+import logo from "../../assets/logo.png";
+function Logo() {
+  return (
+    <>
+      <Image width={100} round={false} src={logo} />
+    </>
+  );
+}
+export default Logo;
--- src/uiKit/image/image.tsx
+++ src/uiKit/image/image.tsx
@@ -1,12 +1,19 @@
-import React from "react";
 import styles from "./style.module.scss";
 interface Props {
-  src: string;
+  src?: string;
   round: boolean;
+  width: number;
 }
 
-function Image({ src, round }: Props) {
-  return <img className={round ? styles.round : ""} src={src} />;
+function Image({ src, round, width }: Props) {
+  return (
+    <div style={{ width: `${width}px`, height: "auto" }}>
+      <img
+        className={`${styles.image} ${round ? styles.round : ""}`}
+        src={src}
+      />
+    </div>
+  );
 }
 
 export default Image;
