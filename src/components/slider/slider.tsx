41d72fdead67600c590cf2f0fa04a3b88c88d831
Tina Homayouni <t.homayouni@vardastapp.com>
2023-06-20 17:06:28 +0330

    add slider

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -1,9 +1,35 @@
-import Image from "../../uiKit/image/image";
-import imageShop from "../../assets/shopImages/dress.jpg";
-function Slider() {
+import Button from "../../uiKit/button/button";
+import styles from "./style.module.scss";
+import colors from "../../assets/_colors.module.scss";
+import { ReactNode } from "react";
+interface Props {
+  children: ReactNode;
+  button: boolean;
+  dot: boolean;
+}
+function Slider({ children, button, dot }: Props) {
+  const next = () => {};
+  const prev = () => {};
   return (
-    <div>
-      <Image width="100%" height="300px" round={false} src={imageShop} />
+    <div className={styles.sliderHolder}>
+      <div className={styles.slider}>{children}</div>
+      {dot && <div className={styles.dot}></div>}
+      {button && (
+        <div className={styles.buttonHolder}>
+          <Button
+            text="<"
+            pen="white"
+            background={colors.purple}
+            onClick={() => alert("prev")}
+          />
+          <Button
+            text=">"
+            pen="white"
+            background={colors.purple}
+            onClick={() => alert("next")}
+          />
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,22 @@
 import Header from "../components/header/header";
 import Slider from "../components/slider/slider";
+import Image from "../uiKit/image/image";
+import imageShop from "../assets/shopImages/dress.jpg";
 
 function Home() {
   return (
     <>
       <Header />
-      <Slider />
+      <Slider button={true} dot={true}>
+        <Image width="100%" height="100%" round={false} src={imageShop} />
+        <Image width="100%" height="100%" round={false} src={imageShop} />
+        <Image width="100%" height="100%" round={false} src={imageShop} />
+        <Image width="100%" height="100%" round={false} src={imageShop} />
+        <Image width="100%" height="100%" round={false} src={imageShop} />
+        <Image width="100%" height="100%" round={false} src={imageShop} />
+      </Slider>
     </>
   );
 }
+
 export default Home;
